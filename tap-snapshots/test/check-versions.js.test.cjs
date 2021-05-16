c07893281a4a8bd84a238917eba2265c189ca47f
Hans Klunder <hans.klunder@gmail.com>
2021-05-16 18:34:07 +0200

    Added check-versions

---
+++ tap-snapshots/test/check-versions.js.test.cjs
@@ -0,0 +1,18 @@
+/* IMPORTANT
+ * This snapshot file is auto-generated, but designed for humans.
+ * It should be checked into source control and tracked carefully.
+ * Re-generate by setting TAP_SNAPSHOT=1 and running tests.
+ * Make sure to inspect the output below.  Do not ignore changes!
+ */
+'use strict'
+exports[`test/check-versions.js TAP Check if version 2.0 is unchanged > schema v2.0 is unchanged 1`] = `
+220e4d47e8dbd8f72a612898429b970bfd652e663569f7202002ceb9767e033a
+`
+
+exports[`test/check-versions.js TAP Check if version 3.0 is unchanged > schema v3.0 is unchanged 1`] = `
+12ba334b36afd9443f1a04cf7fccf09b4a8059d1b7f851faebe91cf60d01f633
+`
+
+exports[`test/check-versions.js TAP Check if version 3.1 is unchanged > schema v3.1 is unchanged 1`] = `
+549386120954be0bdd3778c72ceb376becb83489b4143fe9c26b0b65ebe9891e
+`
