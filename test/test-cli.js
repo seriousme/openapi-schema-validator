8d4cea358c7cfedaac42a36f1712f5880184345a
Hans Klunder <hans.klunder@gmail.com>
2022-04-27 18:49:26 +0200

    Add more cli tests

--- test/test-cli.js
+++ test/test-cli.js
@@ -6,10 +6,24 @@ function localFile(fileName) {
     return new URL(fileName, import.meta.url).pathname;
 }
 
-test("cli does not error", async (t) => {
+test("cli does not error", (t) => {
     t.plan(1);
     const cli = localFile("../bin/validate-api-cli.js");
     const yamlFileName = localFile(`./validation/petstore-openapi.v3.yaml`);
     const result = JSON.parse(execSync(`node ${cli} ${yamlFileName}`));
     t.equal(result.valid, true, "cli validation of petstore spec works")
-})
+});
+
+
+test("cli fails on empty spec",  (t) => {
+    t.plan(1);
+    const cli = localFile("../bin/validate-api-cli.js");
+    const yamlFileName = localFile(`./validation/empty.json`);
+    t.throws(()=> execSync(`node ${cli} ${yamlFileName}`));
+});
+
+test("cli fails on no spec",  (t) => {
+    t.plan(1);
+    const cli = localFile("../bin/validate-api-cli.js");
+    t.throws(()=> execSync(`node ${cli}`));
+});
