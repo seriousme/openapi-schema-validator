b55347106e6754850c5f6c5b5958ec4ef7f93509
Hans Klunder <hans.klunder@gmail.com>
2023-04-30 14:07:33 +0200

    chore: add ajv dynamic ref sniff test

---
+++ test/test-ajv-dynamic-ref-bug.js
@@ -0,0 +1,32 @@
+// sniff test to see of the dynamic ref bug is still present in ajv
+import Ajv2020 from "ajv/dist/2020.js";
+import { test } from "tap";
+
+const schema = {
+	$schema: "https://json-schema.org/draft/2020-12/schema",
+
+	type: "object",
+	properties: {
+		schema: { $dynamicRef: "#meta" },
+	},
+	unevaluatedProperties: false,
+
+	$defs: {
+		schema: {
+			$dynamicAnchor: "meta",
+			type: ["object", "boolean"],
+		},
+	},
+};
+
+const data = {
+	schema: { type: "string" },
+};
+
+test("dynamic ref bug", (t) => {
+	const ajv = new Ajv2020();
+	const validate = ajv.compile(schema);
+	const result = validate(data);
+	t.notOk(result);
+	t.end();
+});
